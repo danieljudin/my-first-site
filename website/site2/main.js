78118056de1c925f2e6ce5bb0de60dda24ed3425
Daniels Judins <danie>
2024-02-17 15:56:59 +0200

    jphibnhiob

--- website/site2/main.js
+++
@@ -1,9 +0,0 @@
-setInterval(() => {
-    let date = new Date();
-    let clock = document.querySelector('.clock');
-    
-    clock.innerHTML = 
-    date.getHours() + ':' +
-    date.getMinutes() + ':' +
-    date.getSeconds();
-},1000)
--- website/site2/meta.js
+++
@@ -1,9 +0,0 @@
-alert("Это проверка на бота, на картинке написана капча. Введите капчу в прямоугольник.")
-
-
-$(".input_text").focus(function(){
-    $(this).prev('.fa').addclass('glowIcon')
-})
-$(".input_text").focusout(function(){
-    $(this).prev('.fa').removeclass('glowIcon')
-})
