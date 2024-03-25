6b4483c16cd1dd836ae3be1b67689b5d18da0544
Daniels Judins <danie>
2024-03-25 19:07:43 +0200

    BubNata

---
+++ website/Messi/messi.js
@@ -0,0 +1,10 @@
+// Add smooth scrolling to nav links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
