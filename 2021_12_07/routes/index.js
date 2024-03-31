88779576a5ad0fd80da3378beb128378d2151c80
Daniels Judins <danie>
2024-03-31 20:14:03 +0300

    deleted

--- 2021_12_07/routes/index.js
+++
@@ -1,7 +0,0 @@
-// routes/index.js
-
-const mainRoutes = require('./main')
-
-module.exports = function (app) {
-    mainRoutes(app);
-}
--- 2021_12_07/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 8000;
-
-require('./routes')(app);
-
-app.listen(port, ()=>{
-    console.log('work on '+port);
-});
